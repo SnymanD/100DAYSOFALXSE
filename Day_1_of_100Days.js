81cfc9591d144c7ca637733eeb8d596a58631ff0
SnmanD <dhlaminisnyman@gmail.com>
2024-03-13 17:50:26 +0200

    99 days to go

---
+++ Day_1_of_100Days.js
@@ -0,0 +1,21 @@
+// Introduction to JavaScript
+// We will be looking at how to print statements in JS and how to declare variables.
+
+console.log("Introduction to JavaScript"); // This is how to print statements to the console.
+
+// You can have as many statements you want to print as possible
+console.log("");
+
+console.log("Adding two numbers in JS");
+
+// Declaring variables - you can declare variables using var, let or const
+console.log('');
+
+let num1 = 12; // variables declared with "let" can be changed or overwritten
+const num2 = 14; // any variable declared with 'const' can never be changed 
+
+num1 = 16; // This will overwrite the contents of a and set it to 16, therefore adding a and be should be equal to 30.
+
+const sum = num1 + num2;
+
+console.log(sum);
---
+++ README.md
@@ -0,0 +1,4 @@
+{
+    I have decided to relearn JavaScript and post my journey with this challenge
+    I will be posting my codes everyday for 100 days, this will be interesting. I will also be adding all codes and files on my Github. Now lets kick start this journey together.
+}

5ba3b4f33a128ad79aec65199bd504d4cc2dc0d2
SnmanD <dhlaminisnyman@gmail.com>
2024-03-18 04:24:03 +0200

    Converting Temperatures

---
+++ Day_6_of_100Days.js
@@ -0,0 +1,19 @@
+// Today I have decided to work on a simple program to change the temperature from
+// Kelvin to Celsius, Fahrenheit and Newton just for interest sake. I will be using JavaScript to write the program.
+
+// Kelvin to Celsius
+const kelvin = 295;
+console.log(` The temperature to convert is ${kelvin} degrees Kelvin.`)
+
+const celsius = kelvin - 273;
+console.log(`The temperature is ${celsius} degrees Celsius.`);
+
+// Celsius to Fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Celsius to Newton
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
